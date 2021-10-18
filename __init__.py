e73f74a642a0f51d1adb51917c2326af21eae3e1
Paul Guo <Paul.Guo@iag.com.au>
2021-10-18 14:47:30 +1100

    setup file tigh up

--- __init__.py
+++ __init__.py
@@ -1,14 +1,14 @@
-from gym.envs.registration import register
-from copy import deepcopy
+# from gym.envs.registration import register
+# from copy import deepcopy
 
-from . import data 
+# from . import data 
 
-df = data.USDGBP5M
-size = 40
-register(
-    id='forex-trading-v0',
-    entry_point='tradinggym.env:tgym',
-    kwargs={'df': deepcopy(df),
-            'window_size':size,
-            'frame_bound':(size,len(df))}
-)
+# df = data.USDGBP5M
+# size = 40
+# register(
+#     id='forex-trading-v0',
+#     entry_point='tradinggym.env:tgym',
+#     kwargs={'df': deepcopy(df),
+#             'window_size':size,
+#             'frame_bound':(size,len(df))}
+# )
